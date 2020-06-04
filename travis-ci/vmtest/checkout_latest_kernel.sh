a6dc44a566fa6cf9413deb16069963ae46154c97
Andrii Nakryiko <andriin@fb.com>
2020-06-03 22:15:45 -0700

    [WIP] Try out another fix to ringbuf selftest
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>

--- travis-ci/vmtest/checkout_latest_kernel.sh
+++ travis-ci/vmtest/checkout_latest_kernel.sh
@@ -6,8 +6,10 @@ CWD=$(pwd)
 LIBBPF_PATH=$(pwd)
 REPO_PATH=$1
 
-BPF_NEXT_ORIGIN=https://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git
-LINUX_SHA=$(cat ${LIBBPF_PATH}/CHECKPOINT-COMMIT)
+BPF_NEXT_ORIGIN=https://github.com/anakryiko/linux.git
+#BPF_NEXT_ORIGIN=https://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git
+#LINUX_SHA=$(cat ${LIBBPF_PATH}/CHECKPOINT-COMMIT)
+LINUX_SHA=c43ace018b98ccb4ff232a74d7b05a462f31e5e8
 
 echo REPO_PATH = ${REPO_PATH}
 echo LINUX_SHA = ${LINUX_SHA}
